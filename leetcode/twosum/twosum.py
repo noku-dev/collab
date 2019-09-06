2e5cf7474f39fcccca1fc7958d0a894ec90c89af
noku-dev <55005410+noku-dev@users.noreply.github.com>
2019-09-06 16:08:10 -0700

    Update twosum.py

--- leetcode/twosum/twosum.py
+++ leetcode/twosum/twosum.py
@@ -1,16 +1,12 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> result;
-        for (auto i=0; i < nums.size(); i++) {
-            for (auto j=i+1; j < nums.size(); j++) {
-                if (target == (nums[i] + nums[j])) {
-                    result.push_back(i);
-                    result.push_back(j);
-                    return result;   
-                }
-            }
-        }
-        return result;
-    }
-};
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
