00ab9b7b53dc233fb5f50c6d8f13895fa1635bab
Eugenio Dols <eudols@users.noreply.github.com>
2019-10-31 23:03:30 -0700

    Update drawingbook.cpp
    Extremely simple mathematical solution glimpsed from the discussion. It works for the testcases, but I am not sure if the math works for all case, it probably does.

--- hackerrank/drawingbook/drawingbook.cpp
+++ hackerrank/drawingbook/drawingbook.cpp
@@ -9,34 +9,8 @@ int pageCount(int n, int p) {
     /*
      * Write your code here.
      */
-    // My intuition is that this is a how many skips of 2 to reach destination problem
-    // but it is from both sides. Right side is a bit more complicated because in 
-    // the end of the book we might have 1 or 2 pages printed. At the beginning of the 
-    // book we know only page 1 is printed on the right side.
-    int distfromend;
-    int pagecount;
-    bool singlelastpage = !(n % 2);
-    bool countfromend = false;
-    if (p == 1 || p == n) {
-         return 0;
-    } else {
-        if (p > n/2) {
-            distfromend = n - p;
-            countfromend = true;
-        } else {
-            distfromend = p;
-        }
-        if (countfromend && singlelastpage) {
-            // I don't like this. It is a way to compensate for the case of singlelast
-            // page and because distfromend = p really is not giving us the distance,
-            // but the page number, i.e. p, not p - 1 which would be what we get when
-            // doing n - p when starting from end. 
-            distfromend++;
-        }
-        pagecount = distfromend / 2 ;
-    }
-    return pagecount;
 
+    return min(p/2,n/2-p/2);
 }
 
 int main()
