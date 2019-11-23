8bf5768c2f2b0c573bab6fbee438ed7db6b07a48
Eugenio Dols <eudols@users.noreply.github.com>
2019-11-22 20:23:10 -0800

    Create printfoobaralternately.cpp

---
+++ leetcode/printfoobaralternately/printfoobaralternately.cpp
@@ -0,0 +1,33 @@
+class FooBar {
+private:
+    int n;
+
+public:
+    FooBar(int n) {
+        this->n = n;
+        m_bar.lock();
+    }
+
+    void foo(function<void()> printFoo) {
+        
+        for (int i = 0; i < n; i++) {
+            m_foo.lock();
+        	// printFoo() outputs "foo". Do not change or remove this line.
+        	printFoo();
+            m_bar.unlock();
+        }
+    }
+
+    void bar(function<void()> printBar) {
+        
+        for (int i = 0; i < n; i++) {
+            m_bar.lock();
+        	// printBar() outputs "bar". Do not change or remove this line.
+        	printBar();
+            m_foo.unlock();
+        }
+    }
+private:
+    std::mutex m_foo;
+    std::mutex m_bar;
+};
