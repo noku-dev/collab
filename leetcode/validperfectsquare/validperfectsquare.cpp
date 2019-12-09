58bfb5a2ce60b0c2768091750c693dfb41d58e1e
Eugenio Dols <eudols@users.noreply.github.com>
2019-12-08 22:52:30 -0800

    Update validperfectsquare.cpp

--- leetcode/validperfectsquare/validperfectsquare.cpp
+++ leetcode/validperfectsquare/validperfectsquare.cpp
@@ -15,3 +15,26 @@ public:
         }        
     }
 };
+
+// Now with binary search
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        unsigned long long sq,mynum;
+        unsigned long long l,r,mid;
+        l = 1;
+        r = mynum = num;
+        do {
+            mid = (l + r)/2;
+            sq = mid * mid;
+            if (sq == mynum) {
+                return true;
+            } else if (sq < mynum) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }    
+        } while (l <= r);
+        return false;
+    }
+};
