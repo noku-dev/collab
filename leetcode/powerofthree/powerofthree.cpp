48dccb58d5fef11b79b5176a2fd72417b03d83ea
Eugenio Dols <eudols@users.noreply.github.com>
2020-01-02 21:14:58 -0800

    Create powerofthree.cpp

---
+++ leetcode/powerofthree/powerofthree.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n == 1) {
+            return true;
+        }
+        if (n < 3) {
+            return false;
+        }
+        while (n > 0) {
+            if (n == 1) {
+                return true;
+            }
+            if (n%3 != 0) {
+                return false;
+            }
+            n /= 3;
+        }
+        return true;
+    }
+};
