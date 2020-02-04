119e03ba3dda0cc72adf3e329f37fcd2e73c4eab
Eugenio Dols <eudols@users.noreply.github.com>
2020-02-03 20:34:42 -0800

    Create pascaltriangle.cpp

---
+++ leetcode/pascaltriangle/pascaltriangle.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res;
+        vector<int> row;
+        if (numRows == 0) return res;
+        row.push_back(1);
+        res.push_back(row);
+        if (numRows == 1) {
+            return res;
+        }
+        row.clear();
+        for (int i = 1 ; i < numRows; i++) {
+            row.push_back(1);
+            for (int j = 1; j < i ; j++) {
+                row.push_back(res[i - 1][j - 1] + res[i -1 ][j]);
+            }
+            row.push_back(1);
+            res.push_back(row);
+            row.clear();
+        }
+        return res;
+        
+    }
+};
